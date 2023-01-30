792b5ca6a657eb8de3e2df064ccc1fdbe5b3d593
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-01-30 15:42:22 +0100

    change favicon and title

--- src/App.js
+++ src/App.js
@@ -8,18 +8,6 @@ function App() {
     // this is JSX
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
         <ImageComponent src={img} alt="mobile image" />
         <h4>Get things done with TODo</h4>
         <p className="sub-text">
